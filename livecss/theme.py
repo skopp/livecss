2bb9170a38aedee0072a207b8b0da7bd54e5eac2
Alexandr Skurikhin Name <a.skurihin@gmail.com>
2012-02-29 02:23:52 +0400

    Remove unused relpath method, better doc for set method

--- livecss/theme.py
+++ livecss/theme.py
@@ -39,10 +39,6 @@ class theme(object):
             return normpath(theme_path)
 
         @property
-        def relpath(cls):
-            return relpath(cls.abspath, SUBLIME_PATH)
-
-        @property
         def dirname(cls):
             return os.path.dirname(cls.abspath)
 
@@ -51,7 +47,9 @@ class theme(object):
             return basename(cls.abspath)
 
         def set(cls, theme_path):
-            """theme: abs or relpath to PACKAGES_PATH"""
+            """Set current theme.
+            :param theme: abs or relpath to SUBLIME_PATH
+            """
             if exists(theme_path):
                 debug('setting new theme %s' % theme_path)
                 cls._settings.set('color_scheme', theme_path)
